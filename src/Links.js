9a1486ba38002689492d217a615d73d314d6d2f7
hamzaazhar1 <hamzaazharx700i@gmail.com>
2022-09-16 19:07:22 +0500

    first commit

---
+++ src/Links.js
@@ -0,0 +1,33 @@
+
+const Links = () => {
+    return ( 
+        <div className="Links">
+            <div>
+                <a href="">SAVE MORE ON APP</a>
+            </div>
+            <div>
+                <a href="">DARAZ AFFILIATE PROGRAM</a>
+            </div>
+            <div>
+                <a href="">SELL ON DARAZ</a>
+            </div>
+            <div>
+                <a href="">CUSTOMER CARE</a>
+            </div>
+            <div>
+                <a href="">TRACK MY ORDER</a>
+            </div>
+            <div>
+                <a href="">LOGIN</a>
+            </div>
+            <div>
+                <a href="">SIGNUP</a>
+            </div>
+            <div>
+                <a href="">زبان تبدیل کریں</a>
+            </div>
+        </div>
+     );
+}
+ 
+export default Links;
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return (  
+            <div className="Navbar">
+                <div className="logo">
+                    <img src="https://icms-image.slatic.net/images/ims-web/3ae67ef5-e5f6-42c3-9a40-993ef9a7bfae.png" alt="" />
+                </div>
+                <div className="search">
+                    <input type="text" placeholder="Search in DARAZ"/>
+                    <button></button>
+                </div>
+                <div className="cart">
+                    
+                </div>
+                
+            </div>       
+    );
+}
+ 
+export default Navbar;
